95a475b82d36c4620db6bf7c74a7a4e08155fa5d
Arunky122 <arunky122@gmail.com>
2020-06-07 23:44:15 +0530

    add state to the component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,30 @@
 import React from 'react';
 
 class CartItem extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            price:60999,
+            title:'Iphone 11 Pro',
+            qty:1,
+            img:''
+        }
+    }
     render(){
+        const{price,title,qty}=this.state;
         return(
             <div className="cart-item">
                <div className="left-block">
-                   <img  alt="" style={style.image}/>
+                   <img  alt="" src="https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-11.jpg"style={style.image}/>
                </div>
                <div className="right-block">
-                   <div style={{fontSize:25, fontWeight:'bold'}}>Phone</div>
-                   <div style={{fontSize:25, color:'green'}}>Rs. 14,999</div>
-                   <div style={{fontSize:25,color:'green'}}>Qty: 1</div>
+                   <div style={{fontSize:25, fontWeight:'bold'}}>{title}</div>
+                   <div style={{fontSize:20, color:'green'}}>Rs.{ price}</div>
+                   <div style={{fontSize:20,color:'green'}}>Qty: {qty}</div>
                    <div className="cart-item-actions">
-
+                       <img  className="action-icons"alt="increase" src="https://image.flaticon.com/icons/svg/1828/1828817.svg"/>
+                       <img className="action-icons" alt="decrease" src="https://image.flaticon.com/icons/svg/929/929430.svg" />
+                       <img  className="action-icons" alt="delete" src="https://image.flaticon.com/icons/svg/1214/1214428.svg"/>
                    </div>
                </div>
 
@@ -23,12 +35,19 @@ class CartItem extends React.Component{
 
 const style ={
     image:{
-        height:100,
-        width:100,
+        height:170,
+        width:120,
         borderRadius:4,
         margin:10,
         backgroundColor:'lightgrey'
     }
 }
+
+// const actionStyle ={
+//     image:{
+//         height:35,
+//         width:35
+//     }
+// }
 
 export default CartItem
